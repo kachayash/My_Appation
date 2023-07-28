96efdbb73e2530ed59fd734423d10a9298474ec0
kachayash <kachayash13@gmail.com>
2023-07-28 13:27:53 +0530

    Login UI Update

--- app/src/main/java/com/example/myapplication/create_singup.java
+++ app/src/main/java/com/example/myapplication/create_singup.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class create_account extends AppCompatActivity {
+public class create_singup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.create_account);
+        setContentView(R.layout.activity_create_singup);
     }
 }
